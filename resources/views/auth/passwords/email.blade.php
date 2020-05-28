0bac481ee20bd52b4acd326afb517c67df4ab55b
Dagbouj-Hatem <dagboujhatem@gmail.com>
2020-05-28 15:55:00 +0100

    fix navbar & footer links

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -39,3 +39,34 @@
     </div>
     <!-- end bootstrap container -->
 @endsection
+
+@push('scripts')
+    <!-- Smooth Scrolling : when clicking on the links in the navbar -->
+    <script type="text/javascript">
+        $(document).ready(function(){
+            // Add smooth scrolling to all links in navbar + footer link
+            $(".navbar a, footer a[href='#home'], footer a[href='#abaut'],  footer a[href='#services'],  footer a[href='#contact']").on('click', function(event) {
+
+                // Make sure this.hash has a value before overriding default behavior
+                if (this.hash !== "") {
+
+                    // Prevent default anchor click behavior
+                    event.preventDefault();
+
+                    // Store hash
+                    var hash = this.hash;
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    location.href = "{{ URL::to('/') }}" + hash;
+
+                } // End if
+            });
+
+
+            // to close navbar on small device
+            $('.navbar-nav>li>a').on('click', function(){
+                $('.navbar-collapse').collapse('hide');
+            });
+
+        })
+    </script>
+@endpush
